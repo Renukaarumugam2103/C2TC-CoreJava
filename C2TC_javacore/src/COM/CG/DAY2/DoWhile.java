26e530245adb7bb2df8b1a4fd9f1169af3076b97
master <renukaarumugam2103@gmail.com>
2023-10-17 12:20:36 +0530

    SECOND COMMIT

---
+++ C2TC_javacore/src/COM/CG/DAY2/DoWhile.java
@@ -0,0 +1,21 @@
+package COM.CG.DAY2;
+
+import java.util.Scanner;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Printing numbers from n to 1 using do while
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		do {
+			System.out.println(n);
+			n--;
+		}
+		while(n>0);
+		
+
+	}
+	
+}
---
+++ C2TC_javacore/src/COM/CG/DAY2/ForEach.java
@@ -0,0 +1,15 @@
+package COM.CG.DAY2;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = {1,3,6,8,9};
+	
+		for(int i:a) {
+			System.out.println(i);
+		}
+
+	}
+
+}

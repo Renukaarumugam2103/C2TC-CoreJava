40f976a1bf63a9b1a93b9ee2bfffc4082c282aa3
master <renukaarumugam2103@gmail.com>
2023-10-22 00:09:20 +0530

    THIRD COMMIT

---
+++ C2TC_javacore/src/COM/CG/DAY3/ConstructorMethod.java
@@ -0,0 +1,19 @@
+package COM.CG.DAY3;
+
+public class ConstructorMethod {
+	String name;
+	int id;
+	//Default constructor
+	ConstructorMethod(){
+		name = "preeti";
+		id = 202;
+	}
+	ConstructorMethod(String name, int id){
+		this.name = name;
+		this.id = id;
+	}
+	void display() {
+		System.out.println("Student Name "+name);
+		System.out.println("student Id: "+id);
+	}
+}
